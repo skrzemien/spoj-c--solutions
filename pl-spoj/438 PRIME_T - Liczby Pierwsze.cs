c812ecb21248452597e5b29e59f99055c2d68aac
Sebastian Krzemień <59866137+skrzemien@users.noreply.github.com>
2022-06-03 23:38:26 +0200

    Create 438 PRIME_T - Liczby Pierwsze.cs

---
+++ pl-spoj/438 PRIME_T - Liczby Pierwsze.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace spoj
+{
+    internal class Program
+    {
+        private static bool PrimeTest(int PrimeNumber)
+        {
+            if (PrimeNumber < 2)
+                return false;
+            for (int i = 2; i < (PrimeNumber/2)+1; i++)
+            {
+                if (PrimeNumber % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {           
+            string InputToPrimesCount = Console.ReadLine();
+            Int32.TryParse(InputToPrimesCount, out int PrimesCount);
+            int[] ArrayOfPrimes = new int[PrimesCount];
+            for (int i = 0; i < PrimesCount; i++)
+            {
+                string InputPrimeNumber = Console.ReadLine();
+                Int32.TryParse(InputPrimeNumber, out int PrimeNumber);
+                ArrayOfPrimes[i] = PrimeNumber;
+            }
+            foreach (int PrimeNumber in ArrayOfPrimes)
+            {
+                if (PrimeTest(PrimeNumber) == true)
+                    Console.WriteLine("TAK");
+                else
+                    Console.WriteLine("NIE");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
