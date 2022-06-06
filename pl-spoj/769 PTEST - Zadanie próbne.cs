dee2ef20d7adb9aaf4058f2b443257d0a5eb6fc9
Sebastian Krzemień <59866137+skrzemien@users.noreply.github.com>
2022-06-06 22:31:45 +0200

    Create 769 PTEST - Zadanie próbne.cs

---
+++ pl-spoj/769 PTEST - Zadanie próbne.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace spoj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            string InputFirstNumber = Console.ReadLine();
+            Int32.TryParse(InputFirstNumber, out int FirstNumber);
+            string InputSecondNumber = Console.ReadLine();
+            Int32.TryParse(InputSecondNumber, out int SecondNumber);
+            Console.WriteLine(FirstNumber+SecondNumber);
+            Console.ReadLine();
+        }       
+    }
+}
