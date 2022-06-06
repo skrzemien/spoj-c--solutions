0a89045e0b806c0ed446be4cb64f053efe09893c
Sebastian Krzemień <59866137+skrzemien@users.noreply.github.com>
2022-06-06 23:22:33 +0200

    Create 549 RNO_DOD - Proste dodawanie.cs
    In progress

---
+++ pl-spoj/549 RNO_DOD - Proste dodawanie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace spoj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            string InputTestsNumber = Console.ReadLine();
+            Int32.TryParse(InputTestsNumber, out int TestsNumber);
+            for (int i = 1; i <= TestsNumber; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }       
+    }
+}
