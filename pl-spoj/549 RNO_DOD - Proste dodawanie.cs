6d2c93791bfdf1ffe5f3dc9575f6553fa799e6a3
Sebastian Krzemień <59866137+skrzemien@users.noreply.github.com>
2022-06-07 12:54:09 +0200

    Update 549 RNO_DOD - Proste dodawanie.cs
    Solved, but not proper for the spoj compilator

--- pl-spoj/549 RNO_DOD - Proste dodawanie.cs
+++ pl-spoj/549 RNO_DOD - Proste dodawanie.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Linq;
 
 namespace spoj
 {
@@ -11,12 +11,17 @@ namespace spoj
             Int32.TryParse(InputTestsNumber, out int TestsNumber);
             for (int i = 1; i <= TestsNumber; i++)
             {
-                string InputNumbers = Console.ReadLine();
-                Int32.TryParse(InputNumbers, out int Numbers);
-                
-
+                string InputNumbersCount = Console.ReadLine();
+                Int32.TryParse(InputNumbersCount, out int NumbersCount);
+                int[] ArrayOfNumbers = new int[NumbersCount];
+                for(int j = 0; j < NumbersCount; j++)
+                {
+                    string InputNumber = Console.ReadLine();
+                    Int32.TryParse(InputNumber, out int Number);
+                    ArrayOfNumbers[j] = Number;
+                }
+                Console.WriteLine(ArrayOfNumbers.Sum());
             }
-
             Console.ReadLine();
         }       
     }
